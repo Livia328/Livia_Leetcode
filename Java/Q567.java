e2bfb2dbae763d303bf3dec799370565e92088e8
Livia328 <374606755@qq.com>
2024-01-29 21:23:59 -0800

    implement Q76

--- Java/Q567.java
+++ Java/Q567.java
@@ -1,5 +1,3 @@
-package Java;
-
 import java.util.*;
 
 public class Q567 {
@@ -8,33 +6,33 @@ public class Q567 {
         for (int i = 0; i < s1.length(); i++) {
             need.put(s1.charAt(i), need.getOrDefault(s1.charAt(i), 0) + 1);
         }
-        Map<Character, Integer> window = new HashMap<>();
         int L = 0, R = 0;
+        Map<Character, Integer> window = new HashMap<>();
         int match = 0;
         while (R < s2.length()) {
             char c = s2.charAt(R);
             R++;
             if (need.containsKey(c)) {
                 window.put(c, window.getOrDefault(c, 0) + 1);
-                if (window.get(c).equals(need.get(c))) {
+                if (need.get(c).equals(window.get(c))) {
                     match++;
                 }
             }
-            while (R - L == s1.length()) {
-                if (match == need.size()) {
-                    return true;
-                }
+            while (R - L > s1.length()) {
                 char d = s2.charAt(L);
                 L++;
                 if (need.containsKey(d)) {
-                    if (window.get(d).equals(need.get(d))) {
+                    if (need.get(d).equals(window.get(d))) {
                         match--;
                     }
-                    window.put(d, window.get(d) - 1);
+                    window.put(d, window.get(d) - 1);  
                 }
             }
-            
+            if (need.size() == match) {
+                return true;
+            }
         }
         return false;
+
     }
 }
---
+++ Java/Q76.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Q76 {
+    public String minWindow(String s, String t) {
+        // count frequency of t
+        Map<Character, Integer> need = new HashMap<>();
+        for (int i = 0; i < t.length(); i++) {
+            need.put(t.charAt(i), need.getOrDefault(t.charAt(i), 0) + 1);
+        }
+        // start sliding window
+        int L = 0, R = 0;
+        Map<Character, Integer> window = new HashMap<>();
+        int match = 0;
+        int minLen = Integer.MAX_VALUE;
+        int start = 0, end = 0;
+        while (R < s.length()) {
+            char c = s.charAt(R);
+            R++;
+            if (need.containsKey(c)) {
+                window.put(c, window.getOrDefault(c, 0) + 1);
+                if (need.get(c).equals(window.get(c))) {
+                    match++;
+                }
+            }
+            // shrink left boundary to get better answer
+            while  (match == need.size()) {
+                if (R - L < minLen) {
+                    minLen = R - L;
+                    start = L;
+                    end = R;
+                }
+                char d = s.charAt(L);
+                L++
+                if (need.containsKey(d)) {
+                    if (need.get(d).equals(window.get(d))) {
+                        match--;
+                    }
+                    window.put(d, window.get(d) - 1);
+                }
+            }
+        }
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
+    }
+}
