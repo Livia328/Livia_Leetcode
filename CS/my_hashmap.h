89752103fafd5c8bcfcf2deaca7a7b260a96b355
Livia328 <374606755@qq.com>
2024-05-01 14:06:12 -0700

    May 1

--- CS/my_hashmap.h
+++ CS/my_hashmap.h
@@ -9,12 +9,12 @@
 //   - (You may consider using these printf statements to debug, but they should be removed from your final version)
 // - You may add any helper functions that you think you need (if any, or otherwise for debugging)
 // ==================================================
+
 #ifndef MY_HASHMAP_T
 #define MY_HASHMAP_T
 
 #include <stdlib.h>
 #include <string.h>
-
 
 // A key value pair
 // This is specifically for a (char*, int) key value pair
@@ -29,24 +29,9 @@ typedef struct node {
     pair_t *kv;          // 'kv' stands for key/value pair
 } node_t;
 
-
-// Simple hash function that will put a key into a bucket
-// is this a good hash function? 
-int stringHash(char *myKey, int numberOfBuckets) {
-    return strlen(myKey) % numberOfBuckets;
-}
-
-// Your hash function goes here
-//  you can rename it and change the signature if you need to
-//      but make sure your hash function starts on line 42 of this file
-int yourHash(char* myKey, int numberOfBuckets) {
-    
-    return 99999;
-}
-
 // Create a function prototype to a function that takes
 // in a char* and an int.
-typedef int(*hashFunctionPointer)(char *, int);
+typedef int (*hashFunctionPointer)(char *, int);
 
 // Chained implementation of a hashmap
 typedef struct hashmap {
@@ -58,7 +43,7 @@ typedef struct hashmap {
     // A function pointer to a hash function
     // The hash_function must take in a 'char*' as a key, and have a
     // second parameter specifying the number of buckets.
-    hashFunctionPointer hashFunction;
+    hashFunctionPointer hashFunction;  
 } hashmap_t;
 
 // A helper function to free the given node.
@@ -88,7 +73,6 @@ hashmap_t *hashmap_create(unsigned int _buckets) {
     map->arrayOfLists = (node_t **) malloc(sizeof(node_t *) * _buckets);
     if (map->arrayOfLists == NULL) {
         free(map);
-
         return NULL;
     }
 
@@ -98,7 +82,7 @@ hashmap_t *hashmap_create(unsigned int _buckets) {
     }
 
     // Setups our hashFunction to point to our stringHash function.
-    map->hashFunction = stringHash;
+    map->hashFunction = betterHash;  // Changed to use betterHash
 
     // Returns the new map that we have created
     return map;
@@ -112,17 +96,14 @@ void hashmap_delete(hashmap_t *_hashmap) {
     if (_hashmap == NULL) {
         return;
     }
-
     for (int i = 0; i < _hashmap->buckets; i++) {
         node_t *iterator = _hashmap->arrayOfLists[i];
-
         while (iterator != NULL) {
             node_t *next_node = iterator->next;
             free_node(iterator);
             iterator = next_node;
         }
     }
-
     free(_hashmap->arrayOfLists);
     free(_hashmap);
 }
@@ -140,18 +121,14 @@ int hashmap_hasKey(hashmap_t *_hashmap, char *key) {
     if (_hashmap == NULL) {
         return -9999;
     }
-
     unsigned int bucket = _hashmap->hashFunction(key, _hashmap->buckets);
-
     node_t *iterator = _hashmap->arrayOfLists[bucket];
     while (iterator != NULL) {
         if (strcmp(iterator->kv->key, key) == 0) {
             return 1;
         }
-
         iterator = iterator->next;
     }
-
     return 0;
 }
 
@@ -166,47 +143,32 @@ void hashmap_insert(hashmap_t *_hashmap, char *key, int value) {
     if (_hashmap == NULL) {
         exit(1);
     }
-
-    // If a key already exists, does nothing and returns.
     if (hashmap_hasKey(_hashmap, key)) {
         return;
     }
-
-    // Creates the new pair.
     pair_t *new_pair = (pair_t *) malloc(sizeof(pair_t));
     if (new_pair == NULL) {
         exit(1);
     }
-
-    // Copies the new key.
     new_pair->key = my_strcpy(key);
     if (new_pair->key == NULL) {
         exit(1);
     }
-
     new_pair->value = value;
-
-    // Creates the new node.
     node_t *new_node = (node_t *) malloc(sizeof(node_t));
     if (new_node == NULL) {
         exit(1);
     }
     new_node->kv = new_pair;
     new_node->next = NULL;
-
-    // Locates the bucket.
     unsigned int bucket = _hashmap->hashFunction(key, _hashmap->buckets);
-
-    // Adds the new node.
     node_t *iterator = _hashmap->arrayOfLists[bucket];
     if (iterator == NULL) {
         _hashmap->arrayOfLists[bucket] = new_node;
-
     } else {
         while (iterator->next != NULL) {
             iterator = iterator->next;
         }
-
         iterator->next = new_node;
     }
 }
@@ -222,21 +184,15 @@ int hashmap_getValue(hashmap_t *_hashmap, char *key) {
     if (_hashmap == NULL) {
         return -9999;
     }
-
-    // May first use hashmap_hasKey() here, but the following way may be more efficient.
-
     unsigned int bucket = _hashmap->hashFunction(key, _hashmap->buckets);
-
     node_t *iterator = _hashmap->arrayOfLists[bucket];
     while (iterator != NULL) {
         if (strcmp(iterator->kv->key, key) == 0) {
-            return iterator->kv->value; 
+            return iterator->kv->value;
         }
-
         iterator = iterator->next;
     }
-
-    return 99999;
+    return 99999; 
 }
 
 // Remove a key from a hashmap
@@ -246,27 +202,20 @@ int hashmap_getValue(hashmap_t *_hashmap, char *key) {
 //  - Search the _hashmap's bucket for the key and then remove it
 // This function should run in average-case constant time
 void hashmap_removeKey(hashmap_t *_hashmap, char *key) {
-
-        // Case 1: the node that needs to be removed is the head node.
-  
-
-        // Case 2: the node that needs to be removed is not the head node.
-
-    if (_hashmap == NULL || key == NULL) return;
-
-    int bucket = _hashmap->hashFunction(key, _hashmap->buckets);
-    node_t **current = &(_hashmap->arrayOfLists[bucket]);
-
-    while (*current != NULL) {
-        if (strcmp((*current)->kv->key, key) == 0) {
-            node_t *next_node = (*current)->next;
-            free_node(*current);
-            *current = next_node;
+    if (_hashmap == NULL) {
+        return;
+    }
+    unsigned int bucket = _hashmap->hashFunction(key, _hashmap->buckets);
+    node_t **indirect = &_hashmap->arrayOfLists[bucket];
+    while (*indirect != NULL) {
+        node_t *current = *indirect;
+        if (strcmp(current->kv->key, key) == 0) {
+            *indirect = current->next;
+            free_node(current);
             return;
         }
-        current = &((*current)->next);
+        indirect = &current->next;
     }
-  return;
 }
 
 // Prints all of the keys in a hashmap
@@ -277,10 +226,8 @@ void hashmap_printKeys(hashmap_t *_hashmap) {
     if (_hashmap == NULL) {
         return;
     }
-
     for (int i = 0; i < _hashmap->buckets; i++) {
         printf("Bucket# %d\n", i);
-
         node_t *iterator = _hashmap->arrayOfLists[i];
         while (iterator != NULL) {
             printf("\tKey=%s\tValue=%d\n", iterator->kv->key, iterator->kv->value);
@@ -289,12 +236,22 @@ void hashmap_printKeys(hashmap_t *_hashmap) {
     }
 }
 
+// A better hash function using the djb2 algorithm
+int betterHash(char *key, int numberOfBuckets) {
+    unsigned long hash = 5381;
+    int c;
+    while (c = *key++) {
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    }
+    return hash % numberOfBuckets;
+}
+
 // A helper function to free the given node.
 static void free_node(node_t *node) {
     if (node == NULL) {
         return;
     }
-
+    
     free(node->kv->key);
     free(node->kv);
     free(node);
@@ -305,9 +262,9 @@ static char *my_strcpy(const char *str) {
     if (new_str == NULL) {
         return NULL;
     }
-
+    
     strcpy(new_str, str);
-
+    
     return new_str;
 }
 
